81072441b4309a64e960261c3e9cc7a5316e9a3f
Karolgdonar-ux <karolgdonar@gmail.com>
2026-07-18 20:38:44 +0100

    Fix Supabase syntax

--- supabase.js
+++ supabase.js
@@ -2,9 +2,11 @@ const SUPABASE_URL = "https://moflesipsnyqrmvsnjaw.supabase.co";
 
 const SUPABASE_ANON_KEY = sb_publishable_6noZ6rOvlvdt9TntaVMGPw_pV1dDl11
 
-const supabase = window.supabase.createClient(window.supabaseClient = supabase;
+const supabase = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
 );
+
+window.supabaseClient = supabase;
 
 console.log("Worldwide Life Matter is connected to Supabase!");
