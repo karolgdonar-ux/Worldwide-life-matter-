fb73e740fc1a0e71af1160f0dbc8533f54f05c18
Karolgdonar-ux <karolgdonar@gmail.com>
2026-07-18 13:56:49 +0100

    Connect website to Supabase

---
+++ supabase.js
@@ -0,0 +1,10 @@
+const SUPABASE_URL = "https://moflesipsnyqrmvsnjaw.supabase.co";
+
+const SUPABASE_ANON_KEY = sb_publishable_6noZ6rOvlvdt9TntaVMGPw_pV1dDl11
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
+
+console.log("Worldwide Life Matter is connected to Supabase!");
