4afa731b6c3767869e6e6acbf609c97f47617731
Karolgdonar-ux <karolgdonar@gmail.com>
2026-07-19 19:54:56 +0100

    Fix Supabase client initialization

--- supabase.js
+++ supabase.js
@@ -1,12 +1,11 @@
 const SUPABASE_URL = "https://moflesipsnyqrmvsnjaw.supabase.co";
-
 const SUPABASE_ANON_KEY = "sb_publishable_6noZ6rOvlvdt9TntaVMGPw_pV1dDl11";
 
-const supabase = window.supabase.createClient(
+const supabaseClient = supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
 );
 
-window.supabaseClient = supabase;
+window.supabaseClient = supabaseClient;
 
-console.log("Worldwide Life Matter is connected to Supabase!");
+console.log(window.supabaseClient);
